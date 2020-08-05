0e70ce6d3fb3d90e56246202c606fac5bf35e408
William Riley <wrileystl@gmail.com>
2020-08-05 10:29:37 -0500

    tab to space

--- GA_Switch_Panel.lua
+++ GA_Switch_Panel.lua
@@ -52,94 +52,94 @@ dataref("xp_avionics2", "sim/cockpit2/electrical/cross_tie", "writable")
 dataref("xp_parkbrake", "sim/flightmodel/controls/parkbrake", "writable")
 
 function switch_panel()
-	-- starter switch_panel
-	-- TODO
-	
-	-- generator
-	if button(btn_generator) then
-		xp_generator = 1
-	else
-		xp_generator = 0
-	end
-	
-	-- battery
-	if button(btn_battery) then
-		xp_battery = 1
-	else
-		xp_battery = 0
-	end
-	
-	-- fuel pump
-	if button(btn_fuel_pump) then
-		xp_fuel_pump_array[0] = 1
-		xp_fuel_pump_array[1] = 1
-		xp_fuel_pump_array[2] = 1
-		xp_fuel_pump_array[3] = 1
-	else
-		xp_fuel_pump_array[0] = 0
-		xp_fuel_pump_array[1] = 0
-		xp_fuel_pump_array[2] = 0
-		xp_fuel_pump_array[3] = 0
-	end
-	XPLMSetDatavi(xp_fuel_pump_ref, xp_fuel_pump_array, 0, 8)
-	
-	-- beacon
-	if button(btn_beacon) then
-		xp_beacon = 1
-	else
-		xp_beacon = 0
-	end
-	
-	-- landing
-	if button(btn_landing) then
-		xp_landing = 1
-	else
-		xp_landing = 0
-	end
-	
-	-- taxi
-	if button(btn_taxi) then
-		xp_taxi = 1
-	else
-		xp_taxi = 0
-	end
-	
-	-- nav
-	if button(btn_nav) then
-		xp_nav = 1
-	else
-		xp_nav = 0
-	end
-	
-	-- strobe
-	if button(btn_strobe) then
-		xp_strobe = 1
-	else
-		xp_strobe = 0
-	end
-	
-	-- pitot
-	if button(btn_pitot) then
-		xp_pitot = 1
-	else
-		xp_pitot = 0
-	end
-	
-	-- avionics
-	if button(btn_avionics) then
-		xp_avionics = 1
+    -- starter switch_panel
+    -- TODO
+    
+    -- generator
+    if button(btn_generator) then
+        xp_generator = 1
+    else
+        xp_generator = 0
+    end
+    
+    -- battery
+    if button(btn_battery) then
+        xp_battery = 1
+    else
+        xp_battery = 0
+    end
+    
+    -- fuel pump
+    if button(btn_fuel_pump) then
+        xp_fuel_pump_array[0] = 1
+        xp_fuel_pump_array[1] = 1
+        xp_fuel_pump_array[2] = 1
+        xp_fuel_pump_array[3] = 1
+    else
+        xp_fuel_pump_array[0] = 0
+        xp_fuel_pump_array[1] = 0
+        xp_fuel_pump_array[2] = 0
+        xp_fuel_pump_array[3] = 0
+    end
+    XPLMSetDatavi(xp_fuel_pump_ref, xp_fuel_pump_array, 0, 8)
+    
+    -- beacon
+    if button(btn_beacon) then
+        xp_beacon = 1
+    else
+        xp_beacon = 0
+    end
+    
+    -- landing
+    if button(btn_landing) then
+        xp_landing = 1
+    else
+        xp_landing = 0
+    end
+    
+    -- taxi
+    if button(btn_taxi) then
+        xp_taxi = 1
+    else
+        xp_taxi = 0
+    end
+    
+    -- nav
+    if button(btn_nav) then
+        xp_nav = 1
+    else
+        xp_nav = 0
+    end
+    
+    -- strobe
+    if button(btn_strobe) then
+        xp_strobe = 1
+    else
+        xp_strobe = 0
+    end
+    
+    -- pitot
+    if button(btn_pitot) then
+        xp_pitot = 1
+    else
+        xp_pitot = 0
+    end
+    
+    -- avionics
+    if button(btn_avionics) then
+        xp_avionics = 1
         xp_avionics2 = 1
-	else
-		xp_avionics = 0
+    else
+        xp_avionics = 0
         xp_avionics2 = 0
-	end
-	
-	-- parkbrake
-	if button(btn_parkbrake) then
-		xp_parkbrake = 1
-	else
-		xp_parkbrake = 0
-	end
+    end
+    
+    -- parkbrake
+    if button(btn_parkbrake) then
+        xp_parkbrake = 1
+    else
+        xp_parkbrake = 0
+    end
 end
 
 do_every_frame("switch_panel()")
