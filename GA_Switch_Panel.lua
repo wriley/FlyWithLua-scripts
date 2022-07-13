ac244e2f10450b4f3615b390c844e1a5e3dc52fa
William Riley <wrileystl@gmail.com>
2022-07-13 11:46:58 -0500

    updates

--- GA_Switch_Panel.lua
+++ GA_Switch_Panel.lua
@@ -32,25 +32,36 @@ else
     if string.match(AIRCRAFT_PATH, "REP") then
         isREP = true
     end
-
+    
+    isMultiEngine = false
+    dataref("num_engines", "sim/aircraft/engine/acf_num_engines")
+    if num_engines > 1 then
+        isMultiEngine = true
+    end
+    
     -- ignition key
-    dataref("xp_ignition", "sim/cockpit2/engine/actuators/ignition_key", "writable")
+    xp_ignition_ref = XPLMFindDataRef("sim/cockpit2/engine/actuators/ignition_key")
+    xp_ignition_array = XPLMGetDatavi(xp_ignition_ref, 0, 8)
 
     -- generator
-    dataref("xp_generator", "sim/cockpit2/electrical/generator_on", "writable")
+    xp_generator_ref = XPLMFindDataRef("sim/cockpit2/electrical/generator_on")
+    xp_generator_array = XPLMGetDatavi(xp_generator_ref, 0, 8)
 
     -- battery
-    dataref("xp_battery", "sim/cockpit/electrical/battery_on", "writable")
+    xp_battery_ref = XPLMFindDataRef("sim/cockpit2/electrical/battery_on")
+    xp_battery_array = XPLMGetDatavi(xp_battery_ref, 0, 8)
 
     -- fuel pump
-    xp_fuel_pump_ref = XPLMFindDataRef("sim/cockpit/engine/fuel_pump_on")
+    xp_fuel_pump_ref = XPLMFindDataRef("sim/cockpit2/engine/actuators/fuel_pump_on")
     xp_fuel_pump_array = XPLMGetDatavi(xp_fuel_pump_ref, 0, 8)
 
     -- beacon
     dataref("xp_beacon", "sim/cockpit2/switches/beacon_on", "writable")
 
     -- landing
-    dataref("xp_landing", "sim/cockpit2/switches/landing_lights_switch", "writable")
+    dataref("xp_landing", "sim/cockpit2/switches/landing_lights_on", "writable")
+    xp_landing_ref = XPLMFindDataRef("sim/cockpit2/switches/landing_lights_switch")
+    xp_landing_array = XPLMGetDatavf(xp_landing_ref, 0, 16)
 
     -- taxi
     dataref("xp_taxi", "sim/cockpit2/switches/taxi_light_on", "writable")
@@ -62,7 +73,8 @@ else
     dataref("xp_strobe", "sim/cockpit2/switches/strobe_lights_on", "writable")
 
     -- pitot
-    dataref("xp_pitot", "sim/cockpit/switches/pitot_heat_on", "writable")
+    dataref("xp_pitot", "sim/cockpit2/ice/ice_pitot_heat_on_pilot", "writable")
+    dataref("xp_pitot2", "sim/cockpit2/ice/ice_pitot_heat_on_copilot", "writable")
 
     -- avionics
     dataref("xp_avionics", "sim/cockpit2/switches/avionics_power_on", "writable")
@@ -79,59 +91,75 @@ else
 
     function switch_panel()
         -- ignition key
-        xp_ignition_set = 0
-        xp_starter_set = 0
-        
-        if button(btn_starter) then
-            xp_starter_set = 1
-        end
+        if not isMultiEngine then
+            xp_ignition_set = 0
+            xp_starter_set = 0
             
-        mag_left = button(btn_mag_left)
-        mag_right = button(btn_mag_right)
-        
-        if mag_left then
-            xp_ignition_set = 1
-        end
-        
-        if mag_right then
-            xp_ignition_set = 2
-        end
-        
-        if mag_left and mag_right then
-            xp_ignition_set = 3
-        end
-        
-        if xp_ignition_set == 3 and xp_starter_set == 1 then
-            xp_ignition = 4
-        else
-            xp_ignition = xp_ignition_set
+            if button(btn_starter) then
+                xp_starter_set = 1
+            end
+                
+            mag_left = button(btn_mag_left)
+            mag_right = button(btn_mag_right)
+            
+            if mag_left then
+                xp_ignition_set = 1
+            end
+            
+            if mag_right then
+                xp_ignition_set = 2
+            end
+            
+            if mag_left and mag_right then
+                xp_ignition_set = 3
+            end
+            
+            if xp_ignition_set == 3 and xp_starter_set == 1 then
+                for i = 0,7,1 do
+                    xp_ignition_array[i] = 4
+                end
+            else
+                for i = 0,7,1 do
+                    xp_ignition_array[i] = xp_ignition_set
+                end
+            end
+            XPLMSetDatavi(xp_ignition_ref, xp_ignition_array, 0, 8)
         end
         
         -- generator
         if button(btn_generator) then
-            xp_generator = 1
+            for i = 0,7,1 do
+                xp_generator_array[i] = 1
+            end
         else
-            xp_generator = 0
+            for i = 0,7,1 do
+                xp_generator_array[i] = 0
+            end
         end
+        XPLMSetDatavi(xp_generator_ref, xp_generator_array, 0, 8)
         
         -- battery
         if button(btn_battery) then
-            xp_battery = 1
+            for i = 0,7,1 do
+                xp_battery_array[i] = 1
+            end
+            
         else
-            xp_battery = 0
+            for i = 0,7,1 do
+                xp_battery_array[i] = 0
+            end
         end
+        XPLMSetDatavi(xp_battery_ref, xp_battery_array, 0, 8)
         
         -- fuel pump
         if button(btn_fuel_pump) then
-            xp_fuel_pump_array[0] = 1
-            xp_fuel_pump_array[1] = 1
-            xp_fuel_pump_array[2] = 1
-            xp_fuel_pump_array[3] = 1
+            for i = 0,7,1 do
+                xp_fuel_pump_array[i] = 1
+            end
         else
-            xp_fuel_pump_array[0] = 0
-            xp_fuel_pump_array[1] = 0
-            xp_fuel_pump_array[2] = 0
-            xp_fuel_pump_array[3] = 0
+            for i = 0,7,1 do
+                xp_fuel_pump_array[i] = 0
+            end
         end
         XPLMSetDatavi(xp_fuel_pump_ref, xp_fuel_pump_array, 0, 8)
         
@@ -144,10 +172,17 @@ else
         
         -- landing
         if button(btn_landing) then
+            for i = 0,15,1 do
+                xp_landing_array[i] = 1
+            end
             xp_landing = 1
         else
+            for i = 0,15,1 do
+                xp_landing_array[i] = 0
+            end
             xp_landing = 0
         end
+        XPLMSetDatavf(xp_landing_ref, xp_landing_array, 0, 16)
         
         -- taxi
         if button(btn_taxi) then
@@ -173,8 +208,10 @@ else
         -- pitot
         if button(btn_pitot) then
             xp_pitot = 1
+            xp_pitot2 = 1
         else
             xp_pitot = 0
+            xp_pitot2 = 0
         end
         
         -- avionics
